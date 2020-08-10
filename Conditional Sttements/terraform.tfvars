6575900a2fae88aa9f16cef9e171c1360c51c671
Amitabha Das <49267338+AmitabhaDazz@users.noreply.github.com>
2020-08-10 19:37:56 +0530

    Add files via upload

---
+++ Conditional Sttements/terraform.tfvars
@@ -0,0 +1,6 @@
+instance_type = "t2.micro"
+ad_ami = "ami-02354e95b39ca8dec"
+access_id = "AKIAWKBYFO27FV3NEBOE"
+secret_id = "XrZQnF09Ekjo2cOLl9KJZpRnFsfgtfDiLFFpPwcQ"
+tags = ["prod","dev"]
+istest = true
---
+++ Conditional Sttements/variable.tf
@@ -0,0 +1,22 @@
+variable access_id {
+    type = string
+    default = "AI"
+}
+variable secret_id {
+    type = string
+    default = "AI"
+}
+variable ad_ami {
+    type = string
+    default = "ami-02354e95b39ca8dec"
+}
+variable instance_type {
+    type = string
+    default = "t2.micro"
+}
+variable "tags" {
+    type = list
+    default = ["dev","prod"]
+}
+variable "istest" {
+}
