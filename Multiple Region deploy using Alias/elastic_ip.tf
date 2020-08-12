5d5d0454cdb1d9323bd2402bf7e628100e705e2e
Amitabha Das <49267338+AmitabhaDazz@users.noreply.github.com>
2020-08-13 00:20:17 +0530

    Update elastic_ip.tf

--- Multiple Region deploy using Alias/elastic_ip.tf
+++ Multiple Region deploy using Alias/elastic_ip.tf
@@ -6,3 +6,11 @@ provider "aws" {
   alias  = "mumbai"
   region = "ap-south-1"
 }
+resource "aws_eip" "ip1" {
+  vpc = "true"
+}
+
+resource "aws_eip" "ip2" {
+  provider = "aws.mumbai"
+  vpc      = "true"
+}
