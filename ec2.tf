9bdbd57d3d0b0627a2247149c52de7175ee72998
Amitabha Das <49267338+AmitabhaDazz@users.noreply.github.com>
2020-08-13 18:52:52 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+
+data "aws_ami" "ad_ami" {
+  most_recent = "true"
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+resource "aws_instance" "awsec2" {
+  ami           = data.aws_ami.ad_ami.id
+  instance_type = "t2.micro"
+}
+
